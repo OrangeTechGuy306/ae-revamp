1b4e1d8bc5df8fb126a9bd189ae7dbb001697b8f
orangetechguy306 <orangetechguy@gmail.com>
2026-03-17 06:57:09 +0100

    fix axios interceptor issue

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://backend.aerenewablesolution.com/';
-// const API_BASE_URL = 'http://localhost:5001/';
+// const API_BASE_URL = 'https://backend.aerenewablesolution.com/';
+const API_BASE_URL = 'http://localhost:5001/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
@@ -14,8 +14,8 @@ const api = axios.create({
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('ae_token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
+        if (token && config.headers) {
+            config.headers.set('Authorization', `Bearer ${token}`);
         }
         return config;
     },
